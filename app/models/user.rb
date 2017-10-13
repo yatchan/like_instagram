c6c38b78a0f1006042643d9982dc80b396c5d283
yatchan <jazzguitar.tokyo@gmail.com>
2017-10-13 14:03:42 +0000

    user.rb不要行削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
   mount_uploader :avatar, AvatarUploader
 #  has_many :pics, dependent: :destroy
+has_many :pics
   has_many :comments, dependent: :destroy
 
  # has_many :relationships, foreign_key: "follower_id", dependent: :destroy
