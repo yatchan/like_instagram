6b38445ec2f2ab9485dd15104f5feda50fbf0725
yatchan <jazzguitar.tokyo@gmail.com>
2017-10-13 11:47:11 +0000

    ユーザー削除ボタン変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
   has_many :pics, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
-  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
-  has_many :followed_users, through: :relationships, source: :followed
-  has_many :followers, through: :reverse_relationships, source: :follower
 
+ # has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+ # has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+ # has_many :followed_users, through: :relationships, source: :followed
+ # has_many :followers, through: :reverse_relationships, source: :follower
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.find_by(provider: auth.provider, uid: auth.uid)
@@ -58,13 +58,13 @@ class User < ActiveRecord::Base
       update_without_password(params, *options)
     end
   end
-  def follow!(other_user)
-    relationships.create!(followed_id: other_user.id)
-  end
-  def following?(other_user)
-    relationships.find_by(followed_id: other_user.id)
-  end
-  def unfollow!(other_user)
-    relationships.find_by(followed_id: other_user.id).destroy
-  end
+#  def follow!(other_user)
+#    relationships.create!(followed_id: other_user.id)
+#  end
+#  def following?(other_user)
+#    relationships.find_by(followed_id: other_user.id)
+#  end
+#  def unfollow!(other_user)
+#    relationships.find_by(followed_id: other_user.id).destroy
+#  end
 end
